7bbabc623c848d911c99366afe3ce50bfe6e62a7
kakikaki-support <info@kakikaki.shop>
2025-02-18 13:04:22 +0800

    Update boxService.js

--- src/services/boxService.js
+++ src/services/boxService.js
@@ -1,4 +1,4 @@
-const API_URL = "https://backend-omega-eight-39.vercel.app/api/box-selection";
+const API_URL = "https://hrp-backend-fain8agpa-kaki-kaki-hrp.vercel.app/api/box-selection";
 
 export const fetchBoxes = async (token) => {
   try {
